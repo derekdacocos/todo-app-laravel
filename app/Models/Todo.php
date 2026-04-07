c788e33d6c7179396524b7ecf5488bec249f1fc1
derekdacocos <de.dacocos24@gmail.com>
2026-04-07 19:58:44 +0800

    Fix mass assignment error by adding fillable fields to Todo model

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+    ];
 }
